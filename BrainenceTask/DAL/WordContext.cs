f0843837919f4795c417468f8b57ba29e1840e73
Yaroslav <zherebetskiy00@gmail.com>
2018-09-21 12:53:15 +0300

    Added DAL

---
+++ BrainenceTask/DAL/WordContext.cs
@@ -0,0 +1,19 @@
+﻿using BrainenceTask.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainenceTask.DAL
+{
+    public class WordContext : DbContext
+    {
+        public WordContext(DbContextOptions<WordContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SearchWord> SearchWords { get; set; }
+    }
+}
