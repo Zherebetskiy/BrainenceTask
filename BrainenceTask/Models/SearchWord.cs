1d1e1317a6a287c65c3f24d729ceb08353a5531a
Yaroslav <zherebetskiy00@gmail.com>
2018-09-21 12:20:00 +0300

    Create project

---
+++ BrainenceTask/Models/SearchWord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainenceTask.Models
+{
+    public class SearchWord
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Sentence { get; set; }
+        public int Amount { get; set; }
+    }
+}
