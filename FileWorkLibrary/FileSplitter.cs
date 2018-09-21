6c6845a21139ca1595c05e8ce448586a6c2c4454
Yaroslav <zherebetskiy00@gmail.com>
2018-09-21 13:35:26 +0300

    Added founding word in centences

---
+++ FileWorkLibrary/FileSplitter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileWorkLibrary
+{
+    public class FileSplitter
+    {
+        readonly string fileUrl;
+
+        public FileSplitter(string fileUrl)
+        {
+            this.fileUrl = fileUrl;
+        }
+
+        public string[] GetSplitedTextBySentence()
+        {
+            string text = File.ReadAllText(string.Concat("@",fileUrl));
+
+            return text.Split('.');
+        }
+    }
+}
---
+++ FileWorkLibrary/WordSearcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FileWorkLibrary
+{
+    class WordSearcher
+    {
+        FileSplitter fileSplitter;
+
+        public WordSearcher(string fileUrl)
+        {
+            fileSplitter = new FileSplitter(fileUrl);
+        }
+
+        public Dictionary<string,int> GetSentencesWithCurrentWord(string word)
+        {
+            Dictionary<string, int> amountOfEntry = new Dictionary<string, int>();
+            string[] splitedText = fileSplitter.GetSplitedTextBySentence();
+
+            foreach (var sentence in splitedText)
+            {
+                var amount = Regex.Matches(sentence, word).Count;
+
+                if (amount != 0)
+                {
+                    amountOfEntry.Add(sentence, amount);
+                } 
+            }
+
+            return amountOfEntry;
+        }
+    }
+}
